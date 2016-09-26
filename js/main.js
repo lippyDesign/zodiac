99db624360b669f0bf9f1e2396d94b23ffdbeb40
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-09-25 22:48:48 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,179 @@
+const signs = [
+    {
+        name: "Aries",
+        thumbnail: 'img/aries-main.png',
+        icon: '&#9800;',
+        dates: '21 March – 20 April',
+        summary: "Aquarius-born are shy and quiet , but on the other hand they can be eccentric and energetic. However, in both cases, they are deep thinkers and highly intellectual people who love helping others. They are able to see without prejudice, on both sides, which makes them people who can easily solve problems. Although they can easily adapt to the energy that surrounds them, Aquarius-born have a deep need to be some time alone and away from everything, in order to restore power. People born under the Aquarius sign, look at the world as a place full of possibilities. Aquarius is an air sign, and as such, uses his mind at every opportunity. If there is no mental stimulation, they are bored and lack a motivation to achieve the best result. The ruling planet of Aquarius, Uranus has a timid, abrupt and sometimes aggressive nature, but it also gives Aquarius visionary quality. They are capable of perceiving the future and they know exactly what they want to be doing five or ten years from now. Uranus also gave them the power of quick and easy transformation, so they are known as thinkers, progressives and humanists. They feel good in a group or a community, so they constantly strive to be surrounded by other people. The biggest problem for Aquarius-born is the feeling that they are limited or constrained. Because of the desire for freedom and equality for all, they will always strive to ensure freedom of speech and movement. Aquarius-born have a reputation for being cold and insensitive persons, but this is just their defence mechanism against premature intimacy. They need to learn to trust others and express their emotions in a healthy way.",
+        romance: "Intellectual stimulation is by far the greatest aphrodisiac for Aquarius. There's nothing that can attract an Aquarius more than an interesting conversation with a person. Openness, communication, imagination and willingness to risk are the qualities that fit well in the perspective of life of this zodiac sign. Their compatibility with other signs can be complex, Integrity and honesty are essential for anyone who wants a long-term relationship with this dynamic person. In love, they are loyal, committed and not at all possessive - they give independence to their partners and consider them as equals.",
+        career: "Aquarius-born bring enthusiasm to the job and have a remarkable ability of exploitation of their imagination for business purposes. Career which enables a development and demonstration of the concept will suit this zodiac sign. Their high intellect combined with their willingness to share their talents, inspires many who work in their environment. Aquarius is a visionary type who likes to engage in activities that aim to make humanity better. When it comes to money, this zodiac sign has a talent to maintain a balance between spending and saving money. Most people born under the sign of Aquarius are well adapted to their feel for style and they are not afraid to show it. It is not uncommon to see an Aquarius boldly dressed in brightly colored suits. Careers such as acting, writing, teaching, photography or piloting, are suitable for this sign. The best environment for them is one that gives them the freedom to solve the problem without strict guidelines. Aquarius is an unconventional type and if given the opportunity to express their talent, can achieve remarkable success.",
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}],
+        traits: [{'Strengths': 'Progressive, original, independent, humanitarian', 'Weaknesses': 'Runs from emotional expression, temperamental, uncompromising, aloof', 'Aquarius likes': 'Fun with friends, helping others, fighting for causes, intellectual conversation, a good listener', 'Aquarius dislikes': 'Limitations, broken promises, being lonely, dull or boring situations, people who disagree with them' }]
+    },
+    {
+        name: "Taurus",
+        thumbnail: '',
+        icon: '&#9801;',
+        dates: '14 May – 19 June',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+        
+},
+    {
+        name: "Gemini",
+        thumbnail: '',
+        icon: '&#9802;',
+        dates: '20 June – 20 July',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Cancer",
+        thumbnail: '',
+        icon: '&#9803;',
+        dates: '21 July – 9 August',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Leo",
+        thumbnail: '',
+        icon: '&#9804;',
+        dates: '10 August – 15 September',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Virgo",
+        thumbnail: '',
+        icon: '&#9805;',
+        dates: '16 September – 30 October',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Libra",
+        thumbnail: '',
+        icon: '&#9806;',
+        dates: '31 October – 22 November',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Scorpio",
+        thumbnail: '',
+        icon: '&#9807;',
+        dates: '23 November – 29 November',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Sagittarius",
+        thumbnail: '',
+        icon: '&#9808;',
+        dates: '23 November – 21 December',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Capricorn",
+        thumbnail: '',
+        icon: '&#9809;',
+        dates: '22 December – 20 January',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Aquarius",
+        thumbnail: '../img/aquarius-main.png',
+        icon: '&#9810;',
+        dates: '21 January – 19 February',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+    {
+        name: "Pisces",
+        thumbnail: '',
+        icon: '&#9811;',
+        dates: '20 February – 20 March',
+        summary: '',
+        romance: '',
+        career: '',
+        stats: [{'Element': 'Air'}, {'Quality': 'Fixed'}, {'Color': 'Blue, Blue-green, Grey, Black'}, {'Day': 'Saturday, Sunday'}, {'Ruler': 'Uranus'}, {'Greatest Overall Compatibility': 'Gemini, Libra'}, {'Best for Marriage and Partnerships': 'Leo'}, {'Lucky Numbers': '4, 8, 13, 17, 22, 26'}, ],
+        traits: []
+    },
+]
+$(document).ready(function() {
+    // Create tab for home button
+    $('.nav').append(`<li class="active"><a data-toggle='tab' href='#home'>Home <i class="fa fa-home" aria-hidden="true"></i></a></li>`);
+    // make navugation tabs work
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    // create navigation tabs for each sign
+    signs.forEach( ({ name, icon }) => {
+        $('.nav').append(`<li><a data-toggle='tab' href='#${name}'>${name} ${icon}</a></li>`);
+    })
+    // create an accordion for each sign
+    signs.forEach( ({ name, thumbnail, dates, summary, romance, career, stats, traits }) => {
+        $('.tab-content').append(`
+            <div id="${name}" class="tab-pane fade">
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="heading${name}">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse${name}" aria-expanded="true" aria-controls="collapse${name}">
+                                    ${name}: <span class="dates">${dates}</span>
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse${name}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading${name}">
+                            <div class="panel-body">
+                                <div class="signHeading">
+                                    <div class="headingImage">
+                                        <img src="${thumbnail}" alt="${name}" class="summaryImage">
+                                    </div>
+                                    <div class="headingText">
+                                        <h1>${name}</h1>
+                                        <h2>${dates}</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    })
+})
